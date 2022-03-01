ea9d7543dfd9c9a1f0b615dad2c35b9d0285a9db
doo <gostbaducking1@nate.com>
2022-03-01 23:50:46 +0900

    first commit

---
+++ src/main/java/com/packtpub/springboot2webapp/SpringBoot2WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.packtpub.springboot2webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2WebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packtpub/springboot2webapp/SpringBoot2WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packtpub.springboot2webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
